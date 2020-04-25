d276030b618aa337a297337a8e0cc83697e43d8a
vitng <vi.ng0204@gmail.com>
2020-04-25 14:04:59 -0700

    initial commit esm

---
+++ js/gotIt.js
@@ -0,0 +1,52 @@
+'use strict';
+function gotData(data){
+
+  // need to retrieve firebase data with val() method
+// this returns an object of all data
+fbData = data.val();
+
+if (fbData) { // check to see if there is something in your database to start
+  console.log('received data:');
+  console.log(fbData);
+
+  // create an array of the post values (if you need to loop through it retaining order of entries)
+  fbDataArray = Object.values(fbData);
+  console.log(fbDataArray);
+} else {
+  console.log('nothing in this folder yet');
+}
+
+}
+
+function errData(err){
+  console.log("error! did not receive data");
+  console.log(err);
+}
+// create a new node
+// the node folder name, id, and object are all passed in as parameters
+
+function createNode(_nodeFolder, _nodeId, _nodeObject) {
+firebase.database().ref(_nodeFolder + '/' + _nodeId).set(_nodeObject);
+
+// call this function to create and seed the folder!
+// createNode(folderName, "seed", {text: "this is to seed folder"});
+// (to test you can just paste it into the web console)
+}
+  //createNode(folderName, "test", {text: "hello"});
+
+  // the update method will update an existing node
+
+  function updateNode(_nodeFolder, _nodeID, _updateObject) {
+  firebase.database().ref(_nodeFolder + '/' + _nodeID).update(_updateObject);
+  // this will update existing key:value pair(s) OR add new ones to your object
+  // so your object might look like:
+  // { existingKey: updatedKeyValue,
+  //   newKey: newValue }
+  // Where the existing key is updated and newKey is added
+  }
+
+  // And this removes an entire node from your folder
+
+  function deleteNode(_nodeFolder, _nodeID) {
+  firebase.database().ref(_nodeFolder + '/' + _nodeID).remove();
+  }
--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,134 @@
+'use strict';
+
+//databaseURL
+//https://console.firebase.google.com/u/0/project/experimentalsocialmedia/database/experimentalsocialmedia/data
+
+let nodeData; // object we will push to firebase
+let fbData; // data we pull from firebase
+let fbDataArray; // firebase data values converted to an array
+let database; // reference to our firebase database
+let folderName = 'messages'; // name of folder you create in db
+let messageInput;
+let sendMessageButton;
+let receiveMessageButton;
+let sendAgainButton;
+let receivedMessage;
+let receiveDiv, sendDiv;
+
+
 function setup() {
-  createCanvas(400, 400);
+
+  noCanvas();
+  //access DOM elements
+  //messageInput = select("#messageInput");
+  messageInput = document.querySelector("#messageInput");
+  sendMessageButton = document.querySelector("#sendMessageButton");
+  receiveMessageButton = document.querySelector("#receiveMessageButton");
+  receivedMessage = document.querySelector("#receivedMessage");
+  sendAgainButton = document.querySelector("#sendAgainButton");
+  receiveDiv = document.querySelector("#receiveDiv");
+  sendDiv = document.querySelector("#sendDiv");
+
+
+  sendMessageButton.addEventListener('click', sendMessage);
+  receiveMessageButton.addEventListener('click', receiveMessage);
+  sendAgainButton.addEventListener('click', sendAgainButton);
+
+  // Initialize firebase
+  // support for Firebase Realtime Database 4 web here: https://firebase.google.com/docs/database/web/start
+  // Copy and paste your config here (replace object commented out)
+  // ---> directions on finding config below
+
+  // paste your config file here
+  let config = {
+    apiKey: "AIzaSyB76T0EByroJWesKHq-6LlPYYDIcM_lkPQ",
+    authDomain: "experimentalsocialmedia.firebaseapp.com",
+    databaseURL: "https://experimentalsocialmedia.firebaseio.com",
+    projectId: "experimentalsocialmedia",
+    storageBucket: "experimentalsocialmedia.appspot.com",
+    messagingSenderId: "576269979759",
+    appId: "1:576269979759:web:13c24a93ed6827dd2b7d9a"
+
+  };
+
+  firebase.initializeApp(config);
+
+  database = firebase.database();
+
+  // this references the folder you want your data to appear in
+  let ref = database.ref(folderName);
+  // **** folderName must be consistant across all calls to this folder
+
+  ref.on('value', gotData, errData);
+
+  // ---> To find your config object:
+  // They will provide it during Firebase setup
+  // or (if your project already created)
+  // 1. Go to main console page
+  // 2. Click on project
+  // 3. On project home page click on name of app under project name (in large font)
+  // 4. Click the gear icon --> it's in there!
+
 }
 
 function draw() {
-  background(220);
+
+}
+
+function sendMessage() {
+  //add a value into text area
+  if (messageInput.value) {
+
+    let timestamp = Date.now();
+    nodeData = {
+
+      messageText: messageInput.value,
+      timestamp: timestamp,
+      //when send it out, it hasn't been received
+      received: false,
+    }
+    //push to firebase
+    createNode(folderName, timestamp, nodeData);
+
+    //createP(`send message : ${nodeData.messageText}`);
+
+    //0 out text area
+    messageInput.value = ''
+
+    sendDiv.style.display = 'none';
+    receiveDiv.style.display = 'block';
+
+  } else {
+    allert("type message first >.<")
+
+  }
+}
+
+function receiveMessage() {
+  for (let i = 0; i < fbDataArray.length; i++) {
+    if (fbDataArray[i].received === false) {
+    //can't see this console -->
+    //console.log("received");
+    //console.log(fbDataArray[i].messageText);
+
+    receivedMessage.innerHTML = fbDataArray[i].messageText;
+
+    updateNode(folderName, fbDataArray[i].timestamp, {received: true});
+
+    receiveMessageButton.style.display = 'none';
+    sendAgainButton.style.display = 'block';
+
+    break;
+
+} else {
+    receivedMessage.innerHTML = "no more messages";
+  //content.log("no more messages");
+}
+}
+}
+
+function sendAgain (){
+
+  receiveDiv.style.display = 'none';
+  sendDiv.style.display = 'block';
 }
